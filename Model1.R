e981a4b2f51ec9ddddf8b17229bedf41a2479282
Regina Manansala <manansalar@gmail.com>
2021-02-25 14:20:53 -0600

    Update - Model 1 (V3; cleaned and annotated)

--- Model1.R
+++ Model1.R
@@ -1,8 +1,8 @@
 #########################################################################
 ## Title: Model 1 EWAS
-## Version: 2
+## Version: 3
 ## Author: Regina Manansala
-## Date: 05-November-2019
+## Date: 06-November-2019
 #########################################################################
 
 library(car)
@@ -32,64 +32,46 @@ M_val <- M_val[, match(ph2$sentrix, colnames(M_val))]
 #########################################################################
 #########################################################################
 
-# test <- M_val[1,] %>% as.matrix() %>% as.data.frame() %>% rename(!!rownames(M_val)[1] := V1) %>% rownames_to_column("sentrix")
-# test2 <- left_join(ph2, test, by = "sentrix")
-# 
-# test_mod <- lm(cg07881041 ~ relevel(MatSD, ref = "1") + AgeMom + relevel(Eth9, ref = "1") + relevel(eduma, ref = "4") + Bcell + CD4T + CD8T + Eos + Mono + Neu + NK, data = test2)
-
-#########################################################################
-#########################################################################
-#########################################################################
-
-## Model 1 - With DNA Methylation data
+# Model 1 - With DNA Methylation data
 
 # Initialize results list
 results <- list()
 
+# Run model for each cpg site
 for(i in 1:nrow(M_val)){
 	x <- M_val[i,]
 	mod <- lm(x ~ relevel(ph2$MatSD, ref = "1") + ph2$AgeMom + relevel(ph2$Eth9, ref = "1") + relevel(ph2$eduma, ref = "4") + ph2$Bcell + ph2$CD4T + ph2$CD8T + ph2$Eos + ph2$Mono + ph2$Neu + ph2$NK)
 	results[[i]] <- summary(mod)$coef[2:5,]
-	if(i %in% c(1, nrow(M_val))) {
+	if(i %in% c(1, nrow(M_val))) { ## If last cpg site, print total runtime
 		print(Sys.time())
 		print("Model 1 - With DNA Methylation")
 	}
 }
 
+# Compile results list into data frame
 names(results) <- rownames(M_val)
 results <- lapply(results, function(x) as.data.frame(x) %>% rownames_to_column('MatSD'))
 big_data <- do.call(rbind, results)
 names(big_data)[2:5] <- c("beta", "se", "t", "pvalue")
 
+# Calculate qvalue and filter by pvalue
 qval <- big_data %>% select(., pvalue) %>% qvalue() %>% `[[`("qvalues") %>% rename(qval = pvalue) %>% rownames_to_column('dnam_id')
 resq <- big_data %>% rownames_to_column('dnam_id') %>% left_join(., qval, by = "dnam_id") %>% subset(., pvalue <= 5e-06)
 #write.table(resq, "model1_results_qfilt.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
 
+# Export model results to RData file
 save(list = ls(pattern = "big_data|qval|resq"), file = "Model1_unstrat.RData")
 
-# which(rownames(M_val) == "cg26709118", arr.ind = TRUE)
-# # 1051, 1992, 5753, 8715
-# foo <- M_val[27587,] %>% as.data.frame() %>% rownames_to_column('sentrix')
-# names(foo)[2] <- "cpg"
-# head(foo[order(foo$cpg),])
-# head(foo[foo$cpg == "-Inf",])
-# 
-# M_val[8715, "200992320006_R05C01"]
-# norm.beta3[8715, "200992320006_R05C01"]
-# 
-# do.call(rbind, lapply(foo, function(x) summary(x)))
-
-# bh <- results %>% as.data.frame() %>% select(., pvalue) %>% p.adjust(., method = "BH")
-
 #########################################################################
 #########################################################################
 #########################################################################
 
-## Model 1a - Stratified by Offspring Sex, With DNA Methylation Data
+# Model 1a - Stratified by Offspring Sex, With DNA Methylation Data
 
 # Gender 1 - Initialize results matrices, Run Models
 results_gen1 <- list()
 
+# Run model for each cpg site
 for(i in 1:nrow(M_val)){
   x <- M_val[i, colnames(M_val) %in% ph2[ph2$gender == 1, "sentrix"]]
   df <- ph2 %>% subset(., gender == 1)
@@ -101,20 +83,26 @@ for(i in 1:nrow(M_val)){
   }
 }
 
+# Compile results list into data frame
 names(results_gen1) <- rownames(M_val)
 results_gen1 <- lapply(results_gen1, function(x) as.data.frame(x) %>% rownames_to_column('MatSD'))
 big_data_gen1 <- do.call(rbind, results_gen1)
 names(big_data_gen1)[2:5] <- c("beta", "se", "t", "pvalue")
 
+# Calculate qvalue and filter by pvalue
 qval_gen1 <- big_data_gen1 %>% as.data.frame() %>% select(., pvalue) %>% qvalue() %>% `[[`("qvalues") %>% rename(qval = pvalue) %>% rownames_to_column('dnam_id')
 resq_gen1 <- big_data_gen1 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_gen1, by = "dnam_id") %>% subset(., pvalue <= 5e-06)
 #write.table(resq_gen1, "model1a_gen1_results_qfilt.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
 
+# Export model results to RData file
 save(list = ls(pattern = "gen1"), file = "Model1_gen1.RData")
+
+#########################################################################
 
 # Gender 2 - Initialize results matrices, Run Models
 results_gen2 <- list()
 
+# Run model for each cpg site
 for(i in 1:nrow(M_val)){
   x <- M_val[i, colnames(M_val) %in% ph2[ph2$gender == 2, "sentrix"]]
   df <- ph2 %>% subset(., gender == 2)
@@ -126,15 +114,18 @@ for(i in 1:nrow(M_val)){
   }
 }
 
+# Compile results list into data frame
 names(results_gen2) <- rownames(M_val)
 results_gen2 <- lapply(results_gen2, function(x) as.data.frame(x) %>% rownames_to_column('MatSD'))
 big_data_gen2 <- do.call(rbind, results_gen2)
 names(big_data_gen2)[2:5] <- c("beta", "se", "t", "pvalue")
 
+# Calculate qvalue and filter by pvalue
 qval_gen2 <- big_data_gen2 %>% as.data.frame() %>% select(., pvalue) %>% qvalue() %>% `[[`("qvalues") %>% rename(qval = pvalue) %>% rownames_to_column('dnam_id')
 resq_gen2 <- big_data_gen2 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_gen2, by = "dnam_id") %>% subset(., pvalue <= 5e-06)
 #write.table(resq_gen2, "model1a_gen2_results_qfilt.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
 
+# Export model results to RData file
 save(list = ls(pattern = "gen2"), file = "Model1_gen2.RData")
 
 #########################################################################
@@ -144,9 +135,9 @@ save(list = ls(pattern = "gen2"), file = "Model1_gen2.RData")
 ## Model 1b - Stratified by Mother's Ethnicity, With DNA Methylation Data
 
 # Ethnicity 1 - Initialize results matrices, Run Models
-
 results_eth1 <- list()
 
+# Run model for each cpg site
 for(i in 1:nrow(M_val)){
   x <- M_val[i, colnames(M_val) %in% ph2[ph2$Eth9 == 1, "sentrix"]]
   df <- ph2 %>% subset(., Eth9 == 1)
@@ -159,21 +150,26 @@ for(i in 1:nrow(M_val)){
 }
 #results_eth1 <- results_gen1 %>% as.data.frame() %>% subset(beta != 0)
 
+# Compile results list into data frame
 names(results_eth1) <- rownames(M_val)
 results_eth1 <- lapply(results_eth1, function(x) as.data.frame(x) %>% rownames_to_column('MatSD'))
 big_data_eth1 <- do.call(rbind, results_eth1)
 names(big_data_eth1)[2:5] <- c("beta", "se", "t", "pvalue")
 
+# Calculate qvalue and filter by pvalue
 qval_eth1 <- big_data_eth1 %>% as.data.frame() %>% select(., pvalue) %>% qvalue() %>% `[[`("qvalues") %>% rename(qval = pvalue) %>% rownames_to_column('dnam_id')
 resq_eth1 <- big_data_eth1 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_eth1, by = "dnam_id") %>% subset(., pvalue <= 5e-06)
 #write.table(resq_eth1, "model1b_eth1_results_qfilt.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
 
+# Export model results to RData file
 save(list = ls(pattern = "eth1"), file = "Model1_eth1.RData")
 
-# Ethnicity 7 - Initialize results matrices, Run Models
+#########################################################################
 
+# Ethnicity 7 - Initialize results matrices, Run Models
 results_eth7 <- list()
 
+# Run model for each cpg site
 for(i in 1:nrow(M_val)){
   x <- M_val[i, colnames(M_val) %in% ph2[ph2$Eth9 == 7, "sentrix"]]
   df <- ph2 %>% subset(., Eth9 == 7)
@@ -185,17 +181,21 @@ for(i in 1:nrow(M_val)){
   }
 }
 
+# Compile results list into data frame
 names(results_eth7) <- rownames(M_val)
 results_eth7 <- lapply(results_eth7, function(x) as.data.frame(x) %>% rownames_to_column('MatSD'))
 big_data_eth7 <- do.call(rbind, results_eth7)
 names(big_data_eth7)[2:5] <- c("beta", "se", "t", "pvalue")
 
+# Calculate qvalue and filter by pvalue
 qval_eth7 <- big_data_eth7 %>% as.data.frame() %>% select(., pvalue) %>% qvalue() %>% `[[`("qvalues") %>% rename(qval = pvalue) %>% rownames_to_column('dnam_id')
 resq_eth7 <- big_data_eth7 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_eth7, by = "dnam_id") %>% subset(., pvalue <= 5e-06)
 #write.table(resq_eth7, "model1b_eth7_results_qfilt.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
 
+# Export model results to RData file
 save(list = ls(pattern = "eth7"), file = "Model1_eth7.RData")
 
+# Combine filtered p-value results into one dataframe and output
 # results2 <- results %>% as.data.frame() %>% subset(pvalue < 5e-06) %>% rownames_to_column('dnam_id') %>% left_join(., qval, by = "dnam_id")
 # results2_gen1 <- results_gen1 %>% as.data.frame() %>% subset(pvalue < 5e-06) %>% rownames_to_column('dnam_id') %>% left_join(., qval_gen1, by = "dnam_id")
 # results2_gen2 <- results_gen2 %>% as.data.frame() %>% subset(pvalue < 5e-06) %>% rownames_to_column('dnam_id') %>% left_join(., qval_gen2, by = "dnam_id")
