47b23b9d0cb4c62bf5b734252a21bcff472de81b
Regina Manansala <manansalar@gmail.com>
2021-02-26 15:52:14 -0600

    Upload Model3 (V4)

--- Model3.R
+++ Model3.R
@@ -1,8 +1,8 @@
 #########################################################################
 ## Title: Model 3 EWAS
-## Version: 3 - Binomial Maternal Education
+## Version: 4 - Updated phenotype file; dichotomized maternal ed
 ## Author: Regina Manansala
-## Date: 21-November-2019
+## Date: 01-January-2020
 #########################################################################
 
 library(car)
@@ -13,17 +13,20 @@ library(qvalue)
 
 load("normalized_betas_kids_for_analysis.RData")
 load("CellCountsBloodgse35069Complete.Rdata")
-ph <- read.csv("bibgwas_07NOV2019.csv")
+ph <- read.csv("bibgwas_27DEC2019.csv")
 #pcs <- fread("PC1-10.txt")
 
 #### Make sure the methylation and phenotype files are sorted exactly the same way.
 counts <- data.frame(counts)
 counts$sentrix <- rownames(counts)
 ph2 <- merge(ph, counts) %>% 
-  mutate(., Eth9 = as.factor(Eth9), edufa = as.factor(edufa), edufa2 = as.factor(edufa2), MatSD = as.factor(MatSD), 
-         eduma = as.factor(eduma), eduma2 = as.factor(eduma2), bn_mtben = as.factor(bn_mtben), quin07 = as.factor(quin07), 
-         empstma = as.factor(empstma), smkpreg = as.factor(smkpreg), alpreg = as.factor(alpreg), gender = as.factor(gender)) 
-         #%>% left_join(., pcs, by = c("sentrix" = "V1"))
+  mutate(., R = as.factor(R), alpreg = as.factor(alpreg), bn_mtben = as.factor(bn_mtben), edufa = as.factor(edufa), 
+  edufa2 = as.factor(edufa2), edufa4c = as.factor(edufa4c), eduma = as.factor(eduma), eduma2 = as.factor(eduma2), 
+  eduma_di = as.factor(eduma_di), eduma4c = as.factor(eduma4c), empstma = as.factor(empstma), Eth9 = as.factor(Eth9), 
+  gender = as.factor(gender), MatSD = as.factor(MatSD), Marstat = as.factor(Marstat), quin07 = as.factor(quin07), 
+  quin07di = as.factor(quin07di), quin073c = as.factor(quin073c), smkpreg = as.factor(smkpreg), hsten = as.factor(hsten), 
+  tenure = as.factor(tenure), hsten2 = as.factor(hsten2), tenown2c = as.factor(tenown2c), tenown3c = as.factor(tenown3c), 
+  finsec = as.factor(finsec))
 betas <- norm.beta3 + 0.0001 
 M_val <- log2(betas/(1-betas))
 M_val <- M_val[, match(ph2$sentrix, colnames(M_val))]
@@ -32,15 +35,15 @@ M_val <- M_val[, match(ph2$sentrix, colnames(M_val))]
 #########################################################################
 #########################################################################
 
-# Model 3 - With DNA Methylation data
+## Model 3 - With DNA Methylation data
 
-#Initialize results list
+# Initialize results list
 results <- list()
 
 # Run model for each cpg site
 for(i in 1:nrow(M_val)){
 	x <- M_val[i,]
-	mod <- lm(x ~ relevel(ph2$eduma2, ref = "2") + ph2$AgeMom + relevel(ph2$Eth9, ref = "1") + ph2$Bcell + ph2$CD4T + ph2$CD8T + ph2$Eos + ph2$Mono + ph2$Neu + ph2$NK)
+	mod <- lm(x ~ relevel(ph2$eduma_di, ref = "2") + ph2$AgeMom + relevel(ph2$Eth9, ref = "1") + ph2$Bcell + ph2$CD4T + ph2$CD8T + ph2$Eos + ph2$Mono + ph2$Neu + ph2$NK)
 	results[[i]] <- summary(mod)$coef[2,]
 	if(i %in% c(1, nrow(M_val))) {
 		print(Sys.time())
@@ -55,11 +58,14 @@ names(big_data)[1:4] <- c("beta", "se", "t", "pvalue")
 
 # Calculate qvalue and filter by pvalue
 qval <- big_data %>% select(., pvalue) %>% qvalue() %>% `[[`("qvalues") %>% rename(qval = pvalue) %>% rownames_to_column('dnam_id')
-resq <- big_data %>% rownames_to_column('dnam_id') %>% left_join(., qval, by = "dnam_id") %>% subset(., pvalue <= 5e-06)
-#write.table(resq, "Model3_results_qfilt.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+resq <- big_data %>% rownames_to_column('dnam_id') %>% left_join(., qval, by = "dnam_id") %>% subset(., qval <= 0.05)
+write.table(resq, "Model3_New_Out/V4/model3_qfilt_v4.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+
+top50 <- big_data %>% rownames_to_column('dnam_id') %>% left_join(., qval, by = "dnam_id") %>% arrange(pvalue)
+write.table(top50[1:50,], "Model3_New_Out/V4/model3_top50_v4.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
 
 # Export model results to RData file
-save(list = ls(pattern = "big_data|qval|resq"), file = "Model3_unstrat.RData")
+save(list = ls(pattern = "results|big_data|qval|resq"), file = "Model3_New_Out/V4/Model3_unstrat_v4.RData")
 
 #########################################################################
 #########################################################################
@@ -74,7 +80,7 @@ results_gen1 <- list()
 for(i in 1:nrow(M_val)){
   x <- M_val[i, colnames(M_val) %in% ph2[ph2$gender == 1, "sentrix"]]
   df <- ph2 %>% subset(., gender == 1)
-  mod <- lm(x ~ relevel(df$eduma2, ref = "2") + df$AgeMom + relevel(df$Eth9, ref = "1") + df$Bcell + df$CD4T + df$CD8T + df$Eos + df$Mono + df$Neu + df$NK)
+  mod <- lm(x ~ relevel(df$eduma_di, ref = "2") + df$AgeMom + relevel(df$Eth9, ref = "1") + df$Bcell + df$CD4T + df$CD8T + df$Eos + df$Mono + df$Neu + df$NK)
   results_gen1[[i]] <- summary(mod)$coef[2,]
   if(i %in% c(1, nrow(M_val))) {
     print(Sys.time())
@@ -89,11 +95,14 @@ names(big_data_gen1)[1:4] <- c("beta", "se", "t", "pvalue")
 
 # Calculate qvalue and filter by pvalue
 qval_gen1 <- big_data_gen1 %>% as.data.frame() %>% select(., pvalue) %>% qvalue() %>% `[[`("qvalues") %>% rename(qval = pvalue) %>% rownames_to_column('dnam_id')
-resq_gen1 <- big_data_gen1 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_gen1, by = "dnam_id") %>% subset(., pvalue <= 5e-06)
-#write.table(resq_gen1, "Model3a_gen1_results_qfilt.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+resq_gen1 <- big_data_gen1 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_gen1, by = "dnam_id") %>% subset(., qval <= 0.05)
+# write.table(resq_gen1, "Model3_New_Out/V4/model3a_gen1_qfilt_v4.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+
+top50_gen1 <- big_data_gen1 %>% rownames_to_column('dnam_id') %>% left_join(., qval, by = "dnam_id") %>% arrange(pvalue)
+# write.table(top50_gen1[1:50,], "Model3_New_Out/V4/model3_gen1_top50_v4.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
 
 # Export model results to RData file
-save(list = ls(pattern = "gen1"), file = "Model3_ed_gen1.RData")
+save(list = ls(pattern = "gen1"), file = "Model3_New_Out/V4/Model3_gen1_v4.RData")
 
 #########################################################################
 
@@ -104,7 +113,7 @@ results_gen2 <- list()
 for(i in 1:nrow(M_val)){
   x <- M_val[i, colnames(M_val) %in% ph2[ph2$gender == 2, "sentrix"]]
   df <- ph2 %>% subset(., gender == 2)
-  mod <- lm(x ~ relevel(df$eduma2, ref = "2") + df$AgeMom + relevel(df$Eth9, ref = "1") + df$Bcell + df$CD4T + df$CD8T + df$Eos + df$Mono + df$Neu + df$NK)
+  mod <- lm(x ~ relevel(df$eduma_di, ref = "2") + df$AgeMom + relevel(df$Eth9, ref = "1") + df$Bcell + df$CD4T + df$CD8T + df$Eos + df$Mono + df$Neu + df$NK)
   results_gen2[[i]] <- summary(mod)$coef[2,]
   if(i %in% c(1, nrow(M_val))) {
     print(Sys.time())
@@ -117,13 +126,16 @@ names(results_gen2) <- rownames(M_val)
 big_data_gen2 <- do.call(rbind, results_gen2) %>% as.data.frame()
 names(big_data_gen2)[1:4] <- c("beta", "se", "t", "pvalue")
 
-# Calculate qvalue and filter by pvalue
+# Compile results list into data frame
 qval_gen2 <- big_data_gen2 %>% as.data.frame() %>% select(., pvalue) %>% qvalue() %>% `[[`("qvalues") %>% rename(qval = pvalue) %>% rownames_to_column('dnam_id')
-resq_gen2 <- big_data_gen2 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_gen2, by = "dnam_id") %>% subset(., pvalue <= 5e-06)
-#write.table(resq_gen2, "Model3a_gen2_results_qfilt.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+resq_gen2 <- big_data_gen2 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_gen2, by = "dnam_id") %>% subset(., qval <= 0.05)
+# write.table(resq_gen2, "Model3_New_Out/V4/model3a_gen2_qfilt_v4.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+
+top50_gen2 <- big_data_gen2 %>% rownames_to_column('dnam_id') %>% left_join(., qval, by = "dnam_id") %>% arrange(pvalue)
+# write.table(top50_gen2[1:50,], "Model3_New_Out/V4/model3_gen2_top50_v4.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
 
 # Export model results to RData file
-save(list = ls(pattern = "gen2"), file = "Model3_ed_gen2.RData")
+save(list = ls(pattern = "gen2"), file = "Model3_New_Out/V4/Model3_gen2_v4.RData")
 
 #########################################################################
 #########################################################################
@@ -138,7 +150,7 @@ results_eth1 <- list()
 for(i in 1:nrow(M_val)){
   x <- M_val[i, colnames(M_val) %in% ph2[ph2$Eth9 == 1, "sentrix"]]
   df <- ph2 %>% subset(., Eth9 == 1)
-  mod <- lm(x ~ relevel(df$eduma2, ref = "2") + df$AgeMom + df$Bcell + df$CD4T + df$CD8T + df$Eos + df$Mono + df$Neu + df$NK)
+  mod <- lm(x ~ relevel(df$eduma_di, ref = "2") + df$AgeMom + df$Bcell + df$CD4T + df$CD8T + df$Eos + df$Mono + df$Neu + df$NK)
   results_eth1[[i]] <- summary(mod)$coef[2,]
   if(i %in% c(1, nrow(M_val))) {
     print(Sys.time())
@@ -154,11 +166,14 @@ names(big_data_eth1)[1:4] <- c("beta", "se", "t", "pvalue")
 
 # Calculate qvalue and filter by pvalue
 qval_eth1 <- big_data_eth1 %>% as.data.frame() %>% select(., pvalue) %>% qvalue() %>% `[[`("qvalues") %>% rename(qval = pvalue) %>% rownames_to_column('dnam_id')
-resq_eth1 <- big_data_eth1 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_eth1, by = "dnam_id") %>% subset(., pvalue <= 5e-06)
-#write.table(resq_eth1, "Model3b_eth1_results_qfilt.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+resq_eth1 <- big_data_eth1 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_eth1, by = "dnam_id") %>% subset(., qval <= 0.05)
+# write.table(resq_eth1, "Model3_New_Out/V4/model3b_eth1_qfilt_v4.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+
+top50_eth1 <- big_data_eth1 %>% rownames_to_column('dnam_id') %>% left_join(., qval, by = "dnam_id") %>% arrange(pvalue)
+# write.table(top50_eth1[1:50,], "Model3_New_Out/V4/model3_eth1_top50_v4.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
 
 # Export model results to RData file
-save(list = ls(pattern = "eth1"), file = "Model3_ed_eth1.RData")
+save(list = ls(pattern = "eth1"), file = "Model3_New_Out/V4/Model3_eth1_v4.RData")
 
 #########################################################################
 
@@ -169,7 +184,7 @@ results_eth7 <- list()
 for(i in 1:nrow(M_val)){
   x <- M_val[i, colnames(M_val) %in% ph2[ph2$Eth9 == 7, "sentrix"]]
   df <- ph2 %>% subset(., Eth9 == 7)
-  mod <- lm(x ~ relevel(df$eduma2, ref = "2") + df$AgeMom + df$Bcell + df$CD4T + df$CD8T + df$Eos + df$Mono + df$Neu + df$NK)
+  mod <- lm(x ~ relevel(df$eduma_di, ref = "2") + df$AgeMom + df$Bcell + df$CD4T + df$CD8T + df$Eos + df$Mono + df$Neu + df$NK)
   results_eth7[[i]] <- summary(mod)$coef[2,]
   if(i %in% c(1, nrow(M_val))) {
     print(Sys.time())
@@ -184,9 +199,11 @@ names(big_data_eth7)[1:4] <- c("beta", "se", "t", "pvalue")
 
 # Calculate qvalue and filter by pvalue
 qval_eth7 <- big_data_eth7 %>% as.data.frame() %>% select(., pvalue) %>% qvalue() %>% `[[`("qvalues") %>% rename(qval = pvalue) %>% rownames_to_column('dnam_id')
-resq_eth7 <- big_data_eth7 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_eth7, by = "dnam_id") %>% subset(., pvalue <= 5e-06)
-#write.table(resq_eth7, "Model3b_eth7_results_qfilt.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+resq_eth7 <- big_data_eth7 %>% as.data.frame() %>% rownames_to_column('dnam_id') %>% left_join(., qval_eth7, by = "dnam_id") %>% subset(., qval <= 0.05)
+# write.table(resq_eth7, "Model3_New_Out/V4/model3b_eth7_qfilt_v4.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+
+top50_eth7 <- big_data_eth7 %>% rownames_to_column('dnam_id') %>% left_join(., qval, by = "dnam_id") %>% arrange(pvalue)
+# write.table(top50_eth7[1:50,], "Model3_New_Out/V4/model3_eth7_top50_v4.txt", col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
 
 # Export model results to RData file
-save(list = ls(pattern = "eth7"), file = "Model3_ed_eth7.RData")
-
+save(list = ls(pattern = "eth7"), file = "Model3_New_Out/V4/Model3_eth7_v4.RData")
